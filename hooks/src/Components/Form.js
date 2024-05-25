37cc1253663752070812f895f424a7cbe98015e8
bishwasgiri <giribishwas4@gmail.com>
2024-05-25 08:35:25 +0545

    useReducer

--- hooks/src/Components/Form.js
+++ hooks/src/Components/Form.js
@@ -1,29 +1,65 @@
-import React, { useEffect } from "react";
+import React, { act, useEffect, useReducer } from "react";
 import { useState } from "react";
 
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 3 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 3 };
+  }
+
+  return { value: "", isValid: false };
+};
+
 const Form = ({ onLoggedIn }) => {
-  const [email, setEmail] = useState("");
+  // const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [emailIsvalid, setEmailIsValid] = useState(false);
-  const [passwordIsValid, setPasswordIsValid] = useState(false);
+  // const [emailIsvalid, setEmailIsValid] = useState(false);
+  // const [passwordIsValid, setPasswordIsValid] = useState(false);
   const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: false,
+  });
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: false,
+  });
 
   const handleEmail = (e) => {
     // console.log("email", e.target.value);
-    setEmail(e.target.value);
+    // setEmail(e.target.value);
     // setFormIsValid(email.includes("@") && password.length > 3);
+    dispatchEmail({ type: "USER_INPUT", val: e.target.value });
   };
 
   const handlePassword = (e) => {
     // console.log("password", e.target.value);
-    setPassword(e.target.value);
+    // setPassword(e.target.value);
     // setFormIsValid(email.includes("@") && password.length > 3);
+    dispatchPassword({ type: "USER_INPUT", val: e.target.value });
   };
 
   const handleForm = (e) => {
     e.preventDefault();
     console.log("form submitted");
-    if (email === "admin@gmail.com" && password === "admin") {
+    if (
+      emailState.value === "admin@gmail.com" &&
+      passwordState.value === "admin"
+    ) {
       console.log("password matched");
       onLoggedIn();
     } else {
@@ -32,25 +68,32 @@ const Form = ({ onLoggedIn }) => {
   };
 
   const handleEmailisValid = (e) => {
-    setEmailIsValid(e.target.value.includes("@"));
+    // setEmailIsValid(emailState.isValid);
+    dispatchEmail({ type: "INPUT_BLUR" });
   };
 
   const handlePasswordisValid = (e) => {
-    setPasswordIsValid(e.target.value.trim().length > 3);
+    // setPasswordIsValid(e.target.value.trim().length > 3);
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
-  // const {email} =
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
 
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("checking form validity");
-      setFormIsValid(email.includes("@") && password.trim().length > 3);
+      setFormIsValid(
+        // emailState.value.includes("@") && passwordState.trim().length > 3
+        // emailState.isValid && passwordState.isValid
+        emailIsValid && passwordIsValid
+      );
     }, 500);
     return () => {
       console.log("cleanup");
       clearTimeout(identifier);
     };
-  }, [email, password]);
+  }, [emailIsValid, passwordIsValid]);
 
   return (
     <div className="w-3/5 mx-auto  flex flex-col items-center mt-20  p-3 ">
@@ -65,13 +108,13 @@ const Form = ({ onLoggedIn }) => {
             placeholder="enter email"
             id="email"
             className={
-              emailIsvalid
+              emailState.isValid
                 ? "outline-none border-gray-700 border p-3"
                 : "outline-none border-red-700 border p-3"
             }
             onChange={handleEmail}
             onBlur={handleEmailisValid}
-            value={email}
+            value={emailState.value}
           />
         </div>
 
@@ -84,13 +127,13 @@ const Form = ({ onLoggedIn }) => {
             placeholder=" enter password"
             id="password"
             className={
-              passwordIsValid
+              passwordState.isValid
                 ? "outline-none border-gray-700 border p-3"
                 : "outline-none border-red-700 border p-3"
             }
             onChange={handlePassword}
             onBlur={handlePasswordisValid}
-            value={password}
+            value={passwordState.value}
           />
         </div>
         <div>
